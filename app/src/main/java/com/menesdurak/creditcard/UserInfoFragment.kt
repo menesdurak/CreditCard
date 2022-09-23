1ce3410dc05cddef97bba4518e16a5f16511a934
Enes <m.enes.durak@gmail.com>
2022-09-23 22:23:13 +0300

    UserInfoFragment.kt added

--- app/src/main/java/com/menesdurak/creditcard/UserInfoFragment.kt
+++ app/src/main/java/com/menesdurak/creditcard/UserInfoFragment.kt
@@ -5,8 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.findNavController
+import com.menesdurak.creditcard.databinding.FragmentUserInfoBinding
 
 class UserInfoFragment : Fragment() {
+
+    private var _binding: FragmentUserInfoBinding? = null
+    private val binding get() = _binding!!
+    private val password = "1234"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,11 +21,26 @@ class UserInfoFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_info, container, false)
+        _binding = FragmentUserInfoBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnToCard.setOnClickListener {
+            if(binding.etPassword.text.toString() == password){
+                it.findNavController().navigate(R.id.action_userInfoFragment_to_frontCardFragment)
+            } else {
+                Toast.makeText(context, "Wrong password! Read hint.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
 
 }
