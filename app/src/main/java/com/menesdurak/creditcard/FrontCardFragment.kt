9745cae4228ed6b863cab3959daa85f21d567c7a
Enes <m.enes.durak@gmail.com>
2022-09-23 16:29:27 +0300

    Implemented TextWatcher to split credit number by four digit

--- app/src/main/java/com/menesdurak/creditcard/FrontCardFragment.kt
+++ app/src/main/java/com/menesdurak/creditcard/FrontCardFragment.kt
@@ -1,6 +1,9 @@
 package com.menesdurak.creditcard
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,11 +15,6 @@ class FrontCardFragment : Fragment() {
 
     private var _binding: FragmentFrontCardBinding? = null
     private val binding get() = _binding!!
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,5 +32,32 @@ class FrontCardFragment : Fragment() {
         binding.btnToBack.setOnClickListener {
             it.findNavController().navigate(R.id.action_frontCardFragment_to_backCardFragment)
         }
+
+        //TextWatcher for creditcard number is taken from:
+        //Hannu Leinonen
+        //https://gist.github.com/hleinone/5b445e5475ca9f8a3bdc6a44998f4edd
+        binding.etCreditCardNumber.addTextChangedListener(object: TextWatcher{
+            private var current = ""
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable) {
+                if (p0.toString() != current) {
+                    val userInput = p0.toString().replace(nonDigits, "")
+                    if (userInput.length <= 16) {
+                        current = userInput.chunked(4).joinToString(" ")
+                        p0.filters = arrayOfNulls<InputFilter>(0)
+                    }
+                    p0.replace(0, p0.length, current, 0, current.length)
+                }
+            }
+        })
+    }
+    companion object {
+        private val nonDigits = Regex("[^\\d]")
     }
 }
